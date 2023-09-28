ffe8dbf9d780dd3fdfd573d393a15efcb133e259
EmoMeza <emeza2019@inf.udec.cl>
2023-09-28 14:07:00 -0300

    ✅

--- frontend/FoodProject/pages/index.vue
+++ frontend/FoodProject/pages/index.vue
@@ -1,18 +1,19 @@
-<script setup lang="ts">
-const runtimeConfig = useRuntimeConfig()
-const appConfig = useAppConfig()
-const route = useRoute()
-</script>
-
-
-
-
 <template>
   <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
-    <p>Runtime config: {{ runtimeConfig.apiSecret }}</p>
-    <p>{{ appConfig.title }}</p>
+    <h1>Fillable Box with Dropdown</h1>
+    <fillable-box></fillable-box>
   </div>
 </template>
+
+<script>
+import FillableBox from '@/components/FillableBox.vue';
+
+export default {
+  components: {
+    FillableBox,
+  },
+};
+</script>
+<style scoped>
+/* Your CSS rules go here */
+</style>
